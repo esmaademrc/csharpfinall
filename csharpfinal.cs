9e3d9d6a35ee61d3db9f23edcd9adcd8819942a3
esmaademrc <95941663+esmaademrc@users.noreply.github.com>
2022-01-25 01:49:22 +0300

    Add files via upload

---
+++ csharpfinal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data.SqlClint;
+
+
+namespace csharp
+{
+public partial class form1=form
+        public form1 ()
+    {
+        InitializeCompanent();
+        sqlconnection connect;
+        try
+        {
+            if(connect.state==connectionstate.closed)
+                connect.open
+        }
+    }
+    }
+}
+    connect.close();
